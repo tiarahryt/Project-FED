945ab4bd7a034dda665713cb84ab6c443a7a8032
Tiara Haryanto <183039563+tiarahryt@users.noreply.github.com>
2025-07-09 14:01:47 +0700

    Add files via upload

---
+++ red love/red love.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+
+int main()
+{
+	float x,y,a;
+	for(y=1.5;y>-1.5;y-=0.1){
+		for(x=-1.5;x<1.5;x+=0.05){
+			a=x*x+y*y-1;
+			putchar(a*a*a-x*x*y*y*y<=0.0?'*':' ');
+		}
+		system("color 0c");
+		putchar('\n');
+	}
+	printf("I Love U <3");
+	
+	return 0;
+}
+
